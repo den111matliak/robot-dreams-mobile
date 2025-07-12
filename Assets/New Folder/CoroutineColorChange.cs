0ce19401d1fa2458fffb04a0266ab0ec2b9a239d
den111matliak <veselkostrug@gmail.com>
2025-07-12 08:39:55 +0300

    Lesson5
    Зміна кольору через корутину

---
+++ Assets/New Folder/CoroutineColorChange.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+public class ColorCycler : MonoBehaviour
+{
+    [SerializeField] private float transitionDuration = 2f;
+
+    private Renderer rend;
+    private Material mat;
+
+    private Color[] colors = new Color[]
+    {
+        Color.blue,
+        Color.yellow,
+        Color.green
+    };
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        mat = rend.material;
+        StartCoroutine(CycleColors());
+    }
+
+    private IEnumerator CycleColors()
+    {
+        int currentIndex = 0;
+
+        while (true)
+        {
+            Color fromColor = colors[currentIndex];
+            Color toColor = colors[(currentIndex + 1) % colors.Length];
+
+            yield return StartCoroutine(LerpColor(fromColor, toColor, transitionDuration));
+
+            currentIndex = (currentIndex + 1) % colors.Length;
+        }
+    }
+
+    private IEnumerator LerpColor(Color from, Color to, float duration)
+    {
+        float timer = 0f;
+
+        while (timer < 1f)
+        {
+            timer += Time.deltaTime / duration;
+            mat.color = Color.Lerp(from, to, timer);
+            yield return null;
+        }
+
+        mat.color = to;
+    }
+}
